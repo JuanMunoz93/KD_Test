d421dc542f5c447ae1263af0251748316d91c6ec
Juan Munoz <juano.mube@gmail.com>
2021-05-18 00:11:26 -0500

    cucumber steps created

---
+++ src/main/java/steps/NewsletterSteps.java
@@ -0,0 +1,25 @@
+package steps;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class NewsletterSteps {
+    @Given("a Hoffner login page")
+    public void a_hoffner_login_page() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("I open a page")
+    public void i_open_a_page() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("I can see a newsletter subscription input")
+    public void i_can_see_a_newsletter_subscription_input() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+
+}
--- src/test/resources/features/newsletterSubscription.feature
+++ src/test/resources/features/newsletterSubscription.feature
@@ -1,6 +1,6 @@
 Feature: Newsletter Subscription
 
-Scenario:
+Scenario: Visibility of newsletter subscription
   Given a Hoffner login page
   When I open a page
   Then I can see a newsletter subscription input
